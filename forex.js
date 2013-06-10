da4c5ab7b2fbb8d32fe26c3df7571e3b943e0e93
Rodney Ramdas <rramdas@gmail.com>
2013-06-10 16:33:41 +0200

    expirementing with integrating forex-node-server

--- forex.js
+++ forex.js
@@ -1,23 +1,48 @@
-var FxRates = new Meteor.Collection('fxrates');
-var Trades = new Meteor.Collection('trades');
+FxRates = new Meteor.Collection('fxrates');
+Trades = new Meteor.Collection('trades');
+Stocks = new Meteor.Collection('stocks');
 
 if (Meteor.isClient) {
+
+  var spread = 0.02;
+
+  function updateRates () {
+      Meteor.call("getRates", function(error, result) {
+      Session.set('ticks', result.data.ticks);
+      Session.set('eurusd', result.data.ticks.EURUSD); 
+      Session.set('eurgbp', result.data.ticks.EURGBP); 
+      Session.set('eurjpy', result.data.ticks.EURJPY); 
+    });
+  }
+
+  Meteor.setInterval(updateRates, 30);
 
   Deps.autorun(function () {
     Meteor.subscribe('fxrates');
     Meteor.subscribe('trades');
+    Meteor.subscribe('stocks');
   });
 
   Template.layout.exchange_rates = function () {
-    return FxRates.find({}).fetch();
+//    return FxRates.find({}).fetch();
+    return Stocks.find({}).fetch();
   }
 
   Template.layout.events({
-
     'click #buyOrder' : function (event, template) {
     }
-
   });
+
+  Template.layout.ticks = function () {
+    return Session.get('ticks');
+  }
+
+  Template.layout.buy_ticks = function () {
+    return Session.get('buy_ticks');
+  }
+
+  Template.layout.direction = function () {
+  }
 
   Template.orderHistory.open_trades = function () {
     return Trades.find({}).fetch();
@@ -28,8 +53,17 @@ if (Meteor.isClient) {
   }
 
   Template.buyOrderModal.events({
+
     'click #buy-button': function (event, template) {
-      //save trade
+
+      var buysell = Template.find('buysell').value();
+      var pair = Template.find('pair').value();
+      var rate = Template.find('rate').value();
+      var risklevel = Template.find('risklevel').value();
+
+      console.log('Entering new trade');
+
+      Trades.insert( { buysell : buysell, pair : pair, rate : rate, risklevel : risklevel } );
     }
   });
 }
@@ -54,7 +88,17 @@ if (Meteor.isServer) {
 
   Meteor.publish ('trades', function () {
     return Trades.find();
-  })
+  });
 
+  Meteor.publish('stocks', function () {
+    return Stocks.find();
+  });
+
+  Meteor.methods ({
+    getRates: function () {
+      this.unblock();
+      return Meteor.http.get('http://localhost:8080/update');
+    }
+  });
 
 }
